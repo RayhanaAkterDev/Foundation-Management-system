290774dfc61127df0dcacd3916953f2f675cec0f
Rayhana Akter Sumaya <rayhanaaktersumaya.dev@gmail.com>
2026-09-19 23:08:40 +0600

    fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'https://stand-for-people-api.onrender.com',
+                target: 'http://127.0.0.1:8000',
                 changeOrigin: true,
                 secure: true,
             },

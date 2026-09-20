36d7995f124822b9d766be6899b66356bf385b8c
Rayhana Akter Sumaya <rayhanaaktersumaya.dev@gmail.com>
2026-09-20 11:32:44 +0600

    Check production mail configuration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-mail-config', function () {
+    return response()->json([
+        'mailer' => config('mail.default'),
+        'host' => config('mail.mailers.smtp.host'),
+        'port' => config('mail.mailers.smtp.port'),
+        'encryption' => config('mail.mailers.smtp.encryption'),
+        'username' => config('mail.mailers.smtp.username'),
+    ]);
+});
